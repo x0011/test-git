6723355e5e4f8a4ec78d2f704b7e950e34f9df49
Y.Panteleev <y-panteleew@yandex.ru>
2021-11-08 21:59:49 +0400

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let resArray = {};
+
+resArray.name = prompt('Введите ваше имя', '');
+resArray.phone = prompt('Введите ваш номер телефона', '');
+resArray.age = Number(prompt('Введите ваш возраст', ''));
+
+if(resArray.age < 18) {
+    alert('Извините, вам запрещен доступ к сайту');
+}else{
+    alert('Добро пожаловать на сайт');
+    document.write("Привет, "+ resArray.name + "!");
+    console.log(typeof(resArray.age));
+}
